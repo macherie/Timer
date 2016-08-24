e5360216bd124eecb1f187d1414c21fea9106404
xin.qi <xin.qi@sim.com>
2016-08-24 14:04:37 +0800

    Timer: Optimize some code

--- timer.c
+++ timer.c
@@ -5,7 +5,10 @@
 #include <string.h>
 
 #define PRELOAD_START_TIMEOUT_MS 1000  // 1 seconds
+
 static void start_wait_timer(void);
+static void setTimer(void);
+static void wait_timeout(void);
 
 typedef struct {
 	int retry_counts;
@@ -18,26 +21,33 @@ static  test_preload preload_retry_cb;
 static void wait_timeout(void)
 {
 	printf("wait_timeout start !!!!!!\n");
-	start_wait_timer();
+	setTimer();
 }
 
 static void start_wait_timer(void)
 {
 	int status;
 	struct sigevent se;
-	struct itimerspec ts;
-	unsigned int timeout_ms = PRELOAD_START_TIMEOUT_MS;
 
 	se.sigev_notify = SIGEV_THREAD;
 	se.sigev_value.sival_ptr = &preload_retry_cb.timer_id;
-	se.sigev_notify_function = wait_timeout;
+	se.sigev_notify_function = (void *)wait_timeout;
 	se.sigev_notify_attributes = NULL;
 
 	status = timer_create(CLOCK_MONOTONIC, &se, &preload_retry_cb.timer_id);
 
 	if (status == 0)
 		preload_retry_cb.timer_created = 1;
-	
+
+	setTimer();
+}
+
+static void setTimer(void)
+{
+	int status;
+	struct itimerspec ts;
+	unsigned int timeout_ms = PRELOAD_START_TIMEOUT_MS;
+
 	if (preload_retry_cb.timer_created == 1) {
 		ts.it_value.tv_sec = timeout_ms/1000;
 		ts.it_value.tv_nsec = 1000000*(timeout_ms%1000);
@@ -47,7 +57,6 @@ static void start_wait_timer(void)
 		if (status == -1)
 			printf("failed \n");
 	}
-
 }
 
 static void stop_wait_timer(void)
@@ -64,7 +73,9 @@ int main(int argc, const char *argv[])
 	memset(&preload_retry_cb, 0, sizeof(preload_retry_cb));
 
 	start_wait_timer();
-	while(1);
 
+	while(1) {
+		sleep(10);
+	}
 	return 0;
 }
